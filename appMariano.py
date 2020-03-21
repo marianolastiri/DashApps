533941ba5762604b26a2ef053124df8df934a938
marianolastiri <44900130+marianolastiri@users.noreply.github.com>
2020-03-20 23:29:41 -0300

    more apps added, appMariano updated

--- appMariano.py
+++ appMariano.py
@@ -1,9 +1,14 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 import yfinance as yf
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+server = app.server
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
@@ -12,42 +17,34 @@ colors = {
     'text': '#557'
 }
 
-def get_data(ticker):
-    datos = yf.download(ticker)
-    return datos
-
-ggal = get_data('ggal')
-
-app.layout = html.Div(style = {'backgroundColor': colors['background']},
+app.layout = html.Div(style = {'backgroundColor': colors['background'], 'textAlign': 'center'},
 children = [
     html.H1(
         children = "Datos bursátiles",
         style = {
-        'textAlign': 'center',
         'color': colors['text']
         }
     ),
-    dcc.Graph(
-        figure = {
-            'data': [
-                dict(
-                    y = ggal['Close']
-                )
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        },
-    ),
+    dcc.Graph(id = 'grafico'),
 
     html.Label('Ingrese ticker'),
-    dcc.Input(value = 'GGAL', type = 'text'),
+    dcc.Input(id = 'ticker', value = None, type = 'text', debounce = True),
+    #html.Button(id = 'submit-button', type = 'submit', children = 'Submit')
 
-])
+]
+)
+
+@app.callback(
+    Output('grafico', 'figure'),
+    [Input('ticker', 'value')]
+)
+
+def get_data(ticker):
+    if ticker is None:
+        raise dash.exceptions.PreventUpdate
+
+    datos = yf.download(ticker)
+    return {'data': [dict(y = datos['Close'])], 'layout': {'plot_bgcolor': colors['background'], 'paper_bgcolor': colors['background'], 'font': {'color': colors['text']}}}
 
 if __name__ == '__main__':
     app.run_server(debug = True)
